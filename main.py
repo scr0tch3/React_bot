e23785a80f5176ebc5cbe043ed6b577b6644f15b
scr0tch3 <daniilhorev0987@gmail.com>
2024-02-12 16:49:37 +0300

    0.1

---
+++ main.py
@@ -0,0 +1,115 @@
+import disnake
+from disnake.ext import commands
+import random
+import os
+
+TOKEN = "MTIwNjI0MjExNjgyOTg0NzU5Mg.Gg4unf.yPK25jXRe1qC2TF_T-KOM19--GVlG3OHQUkAXk"
+
+bot = commands.Bot(command_prefix="!", help_command=None, intents=disnake.Intents.all(), test_guilds=[1159839672550297675]) 
+
+@bot.event
+async def on_ready():
+    print(f"Bot {bot.user} is ready to work!")
+    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.playing, name="Чебупели"))
+
+@bot.command()
+async def злиться(ctx):
+    directory = "angry"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} злится:", file=picture)
+
+@bot.command()
+async def улыбаться(ctx):
+    directory = "smiles"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} улыбается:", file=picture)
+
+@bot.command()
+async def укусить(ctx):
+    directory = "bite"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} кусает  :", file=picture)
+
+@bot.command()
+async def плакать(ctx):
+    directory = "cry"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} плачет:", file=picture)
+
+@bot.command()
+async def танцевать(ctx):
+    directory = "dance"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} танцует:", file=picture)
+
+@bot.command()
+async def обнять(ctx):
+    directory = "hug"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} обнимает  :", file=picture)
+
+@bot.command()
+async def поцеловать(ctx):
+    directory = "kiss"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} целует  :", file=picture)
+
+@bot.command()
+async def смеятся(ctx):
+    directory = "laugh"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} смеёться:", file=picture)
+
+@bot.command()
+async def гладить(ctx):
+    directory = "pat"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} гладит  :", file=picture)
+
+@bot.command()
+async def ударить(ctx):
+    directory = "slap"
+    files = os.listdir(directory)
+    random_image = random.choice(files)
+    file_path = os.path.join(directory, random_image)
+    with open(file_path, 'rb') as f:
+        picture = disnake.File(f)
+        await ctx.send(f"{ctx.author.mention} бьёт :", file=picture)
+
+bot.run(TOKEN)
